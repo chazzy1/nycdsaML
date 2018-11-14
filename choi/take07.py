be91c42831a7eb00d6d8613e8c10b8c4578d2c2b
SangYeon Choi <hellotheresy@gmail.com>
2018-11-14 01:14:53 -0500

    new transform

--- choi/take07.py
+++ choi/take07.py
@@ -1,20 +1,25 @@
+
 import sys
+import warnings
 sys.path.append('../')
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Lasso, LassoCV, ElasticNet,Ridge
-
-
-
+from sklearn.svm import SVR, LinearSVR
 from sklearn.ensemble import RandomForestRegressor
 from mlxtend.regressor import StackingRegressor
 import xgboost as xgb
 import lightgbm as lgb
 
 pd.options.mode.chained_assignment = None
-from utils.transform import *
+#from utils.transform import *
+from utils.transformChoi import *
+
 
 def main():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
     """
     load data
     """
@@ -24,7 +29,13 @@ def main():
     """
     Remove Outliers
     """
-    outliers = train_set[(train_set['GrLivArea'] > 4000) & (train_set['SalePrice'] < 300000)].index
+    #outliers = train_set[(train_set['GrLivArea'] > 4000) & (train_set['SalePrice'] < 300000)].index
+
+    outliers = train_set[train_set['GrLivArea'] > 4500].index
+    print(outliers)
+    outliers = [197, 523, 691, 854, 1182, 1298]
+
+
     train_set.drop(outliers, inplace=True)
 
     """
@@ -48,55 +59,85 @@ def main():
 
     combined_data = pd.concat((train_set, test_set))
 
+    # total = combined_data.isnull().sum().sort_values(ascending=False)
+    # percent = (combined_data.isnull().sum() / combined_data.isnull().count()).sort_values(ascending=False)
+    # missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    #
+    # # drop columns over 40% what fill with NAN
+    # features_to_drop = missing_data[missing_data['Percent'] > 0.4].index
+    # features_to_drop = features_to_drop.union(['GarageArea'])
+    # combined_data.drop(features_to_drop, axis=1, inplace=True)
+
 
     """
     create data transform pipeline
     """
     transform_pipeline = Pipeline(steps=[
+        ('FeatureDropper', FeatureDropper()),
         ('NaNFixer', NaNFixer()),
+        #('NaNFixer', NaNFixerDetail()),
         ('SkewFixer', SkewFixer()),
         ('Scaler', Scaler()),
-        ('FeatureDropper', FeatureDropper()),
         ('Dummyfier', Dummyfier()),
         #('TrainDataSeparator', TrainDataSeparator(train_set_rows=train_set_rows)),
     ])
+
+    ## 270 Columns
+
 
     transformed_data = transform_pipeline.transform(combined_data)
     train_data = transformed_data[:train_set_rows]
     predict_data = transformed_data[train_set_rows:]
 
+    # from sklearn.decomposition import PCA, KernelPCA
+    # pca = PCA(n_components=135)
+    # train_data = pca.fit_transform(train_data)
+    # predict_data = pca.transform(predict_data)
+
     """
     try various regressors
     """
 
-    rf_param = {
-        'n_estimators': [10,12],
-        'max_depth': [3],
-        'n_jobs': [-1]
-    }
+    #
+    # rf_param = {
+    #     'n_estimators': [10, 12],
+    #     'max_depth': [3],
+    #     'n_jobs': [-1]
+    # }
 
+    rf_param = {
+       # 'bootstrap': [True],
+        'max_depth': [3,4,5],
+        'min_samples_leaf': [3, 4, 5],
+        'n_estimators': [5,7,10]
+    }
     ls_param = {'alpha': [0.0001,0.0002,0.0003,0.0004,0.0005],
                 'max_iter':[10000],"normalize": [True, False] }
 
-    elnet_param = {'alpha':[0.0008,0.004,0.005],
+    elnet_param = {'alpha':[0.0008,0.003,0.004,0.005],
                    'l1_ratio':[0.08,0.1,0.3],
                    'max_iter':[10000]}
 
     ridge_param = {'alpha': [35, 40, 45, 50, 55, 60, 65, 70, 80, 90]}
 
-    # gbm_param = {"n_estimators": [1000],
-    #              'min_child_weight': [1, 5, 10],
-    #              'gamma': [0.1, 0.5, 1, 1.5, 2, 5],
-    #              'subsample': [0.6, 0.8, 1.0],
-    #              'colsample_bytree': [0.6, 0.8, 1.0],
-    #              'max_depth': [3, 4, 5],
-    #              'eta': [0.01],
-    #              'eval_metric': ['mae']
-    #              }
-    #
-
-    gbm_param = {"n_estimators": [1000]
+    svr_param ={ 'gamma': [1e-08, 1e-09],
+                 'C': [100000, 110000],
+                 'epsilon': [1, 0.1, 0.01]
                  }
+
+    gbm_param = {"n_estimators": [1000],
+                 'min_child_weight': [1, 5],
+                 'gamma': [0.1, 0.2],
+                 'subsample': [0.6],
+                 'colsample_bytree': [0.6],
+                 'max_depth': [3, 4],
+                 'eta': [0.01],
+                 'eval_metric': ['mae']
+                 }
+
+
+    # gbm_param = {"n_estimators": [1000]
+    #              }
 
     lgb_params = {
         'objective': ['regression'],
@@ -104,36 +145,34 @@ def main():
         'max_depth': [8],
         'bagging_seed': [3],
         'boosting_type': ['gbdt']
-        # ,
-        # 'min_sum_hessian_in_leaf' : [100],
-        # 'learning_rate': np.linspace(0.05, 0.1, 3),
-        # 'bagging_fraction': np.linspace(0.7, 0.9, 3),
-        # 'bagging_freq': np.linspace(30, 50, 3, dtype='int'),
-        # 'max_bin': [15, 63, 255],
+        ,
+        'min_sum_hessian_in_leaf' : [100],
+        'learning_rate': np.linspace(0.05, 0.1, 2),
+        'bagging_fraction': np.linspace(0.7, 0.9, 2),
+        'bagging_freq': np.linspace(30, 50, 3, dtype='int'),
+        'max_bin': [15, 63],
     }
 
-
-    # grid(SVR()).grid_get(X_scaled,y_log,{'C':[11,13,15],'kernel':["rbf"],"gamma":[0.0003,0.0004],"epsilon":[0.008,0.009]})
-    # param_grid={'alpha':[0.2,0.3,0.4], 'kernel':["polynomial"], 'degree':[3],'coef0':[0.8,1]}
-    # grid(KernelRidge()).grid_get(X_scaled,y_log,param_grid)
-
     rf = get_best_estimator(train_data, y_train_values, estimator=RandomForestRegressor(),
-                            params = rf_param)
+                            params = rf_param,n_jobs=4)
     elnet = get_best_estimator(train_data, y_train_values, estimator=ElasticNet(),
-                             params= elnet_param )
-
+                             params= elnet_param,n_jobs=4 )
     lso = get_best_estimator(train_data, y_train_values, estimator=Lasso(),
-                             params=ls_param)
+                             params=ls_param,n_jobs=4)
     rdg = get_best_estimator(train_data, y_train_values, estimator=Ridge(),
-                             params=ridge_param)
+                             params=ridge_param,n_jobs=4)
+    svr = get_best_estimator(train_data, y_train_values, estimator=SVR(),
+                             params=svr_param, n_jobs=4)
 
-    gbm = get_best_estimator(train_data, y_train_values, estimator=xgb.XGBRegressor(),
-                             params=gbm_param)
-    lbm = get_best_estimator(train_data, y_train_values, estimator=lgb.LGBMRegressor(),
-                             params=lgb_params)
+    # gbm = get_best_estimator(train_data, y_train_values, estimator=xgb.XGBRegressor(),
+    #                          params=gbm_param, n_jobs=4)
+    # lbm = get_best_estimator(train_data, y_train_values, estimator=lgb.LGBMRegressor(),
+    #                          params=lgb_params, n_jobs=4)
+
 
     model = StackingRegressor(
-        regressors=[rf, elnet, lso, rdg, gbm, lbm ],
+       # regressors=[rf, elnet, lso, rdg, gbm, lbm ],
+        regressors=[rf, elnet, lso ,rdg,svr ],
         meta_regressor=Lasso(alpha=0.0005)
     )
 
@@ -157,7 +196,8 @@ def main():
 
     """" Ensemble Weights """
     from scipy.optimize import minimize
-    regressors = [rf, elnet, lso, rdg, gbm, lbm]
+    #regressors = [rf, elnet, lso, rdg, gbm, lbm]
+    regressors = [rf, elnet, lso, rdg, svr ]
 
     predictions = []
     for clf in regressors:
@@ -180,6 +220,7 @@ def main():
     print('Result Assessment: {message_algo}'.format(message_algo=res['message']))
     print('Ensemble Score: {best_score}'.format(best_score=res['fun']))
     print('Best Weights: {weights}'.format(weights=res['x']))
+
 
 
 if __name__== "__main__":
---
+++ utils/transformChoi.py
@@ -0,0 +1,241 @@
+import sys
+sys.path.append('../')
+
+import pandas as pd
+import numpy as np
+from math import sqrt
+from utils.utils import *
+from sklearn.pipeline import Pipeline
+from sklearn.base import TransformerMixin
+
+from sklearn.metrics import mean_squared_error, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn import preprocessing
+
+from scipy.stats import skew
+from scipy.special import boxcox1p
+
+
+class NaNFixer(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        X["LotAreaCut"] = pd.qcut(X.LotArea, 10)
+        X['LotFrontage'] = X.groupby(['LotAreaCut', 'Neighborhood'])['LotFrontage'].transform(
+            lambda x: x.fillna(x.median()))
+
+        X.drop('LotAreaCut', axis=1, inplace=True)
+        categorical_with_nan = get_columns_with_nan(X[get_categorical_columns(X)])
+
+        for col in categorical_with_nan:
+            X[col].fillna(X[col].mode()[0], inplace=True)
+
+        numerical_with_nan = get_columns_with_nan(X[get_numeric_columns(X)])
+
+        for col in numerical_with_nan:
+            X[col].fillna(0, inplace=True)
+
+        X["MSSubClass"] = X["MSSubClass"].astype(str)
+
+        X["TotalHouse"] = X["TotalBsmtSF"] + X["1stFlrSF"] + X["2ndFlrSF"]
+        X["Rooms"] = X["FullBath"] + X["TotRmsAbvGrd"]
+        X["Bsmt"] = X["BsmtFinSF1"] + X["BsmtFinSF2"] + X["BsmtUnfSF"]
+        X['TotBathrooms']  = X['FullBath']  + (X['HalfBath'] * 0.5) + X['BsmtFullBath'] + (X['BsmtHalfBath'] * 0.5)
+
+        X['Age'] = X['YrSold'] - X['YearRemodAdd']
+        #X['isRemod'] = np.where(X['YearRemodAdd'] == X['YearBuilt'], '0', '1') #0=No Remodeling, 1=Remodeling
+        #X['isNew'] = np.where(X['YrSold'] == X['YearBuilt'], 1, 0)
+
+        ## Legacy
+        # X["PorchArea"] = X["OpenPorchSF"] + X["EnclosedPorch"] + X["3SsnPorch"] + X["ScreenPorch"]
+        # X["TotalPlace"] = X["TotalBsmtSF"] + X["1stFlrSF"] + X["2ndFlrSF"]  + X["OpenPorchSF"] \
+        #                   + X[
+        #     "EnclosedPorch"] + X["3SsnPorch"] + X["ScreenPorch"]
+
+        # X["Bsmt"] = X["BsmtFinSF1"] + X["BsmtFinSF2"] + X["BsmtUnfSF"]
+        # X["+_TotalHouse_OverallQual"] = X["TotalHouse"] * X["OverallQual"]
+        # X["+_GrLivArea_OverallQual"] = X["GrLivArea"] * X["OverallQual"]
+        # X["+_oMSZoning_TotalHouse"] = X["oMSZoning"] * X["TotalHouse"]
+        # X["+_oMSZoning_OverallQual"] = X["oMSZoning"] + X["OverallQual"]
+
+        #
+        # NumStr = ["MSSubClass", "BsmtFullBath", "BsmtHalfBath", "HalfBath", "BedroomAbvGr", "KitchenAbvGr", "MoSold",
+        #           "YrSold", "YearBuilt", "YearRemodAdd", "LowQualFinSF", "GarageYrBlt"]
+        #
+        # for col in NumStr:
+        #     X[col] = X[col].astype(str)
+        #
+        # X["MSSubClass"] = X.MSSubClass.map({'180': 1,
+        #                                      '30': 2, '45': 2,
+        #                                      '190': 3, '50': 3, '90': 3,
+        #                                      '85': 4, '40': 4, '160': 4,
+        #                                      '70': 5, '20': 5, '75': 5, '80': 5, '150': 5,
+        #                                      '120': 6, '60': 6})
+
+        # X["MSZoning"] = X.MSZoning.map({'C (all)': 1, 'RH': 2, 'RM': 2, 'RL': 3, 'FV': 4})
+        #
+        # X["oNeighborhood"] = X.Neighborhood.map({'MeadowV': 1,
+        #                                          'IDOTRR': 2, 'BrDale': 2,
+        #                                          'OldTown': 3, 'Edwards': 3, 'BrkSide': 3,
+        #                                          'Sawyer': 4, 'Blueste': 4, 'SWISU': 4, 'NAmes': 4,
+        #                                          'NPkVill': 5, 'Mitchel': 5,
+        #                                          'SawyerW': 6, 'Gilbert': 6, 'NWAmes': 6,
+        #                                          'Blmngtn': 7, 'CollgCr': 7, 'ClearCr': 7, 'Crawfor': 7,
+        #                                          'Veenker': 8, 'Somerst': 8, 'Timber': 8,
+        #                                          'StoneBr': 9,
+        #                                          'NoRidge': 10, 'NridgHt': 10})
+        #
+        # X["Condition1"] = X.Condition1.map({'Artery': 1,
+        #                                      'Feedr': 2, 'RRAe': 2,
+        #                                      'Norm': 3, 'RRAn': 3,
+        #                                      'PosN': 4, 'RRNe': 4,
+        #                                      'PosA': 5, 'RRNn': 5})
+        #
+        # X["BldgType"] = X.BldgType.map({'2fmCon': 1, 'Duplex': 1, 'Twnhs': 1, '1Fam': 2, 'TwnhsE': 2})
+        #
+        # X["HouseStyle"] = X.HouseStyle.map({'1.5Unf': 1,
+        #                                      '1.5Fin': 2, '2.5Unf': 2, 'SFoyer': 2,
+        #                                      '1Story': 3, 'SLvl': 3,
+        #                                      '2Story': 4, '2.5Fin': 4})
+        #
+        # X["Exterior1st"] = X.Exterior1st.map({'BrkComm': 1,
+        #                                        'AsphShn': 2, 'CBlock': 2, 'AsbShng': 2,
+        #                                        'WdShing': 3, 'Wd Sdng': 3, 'MetalSd': 3, 'Stucco': 3, 'HdBoard': 3,
+        #                                        'BrkFace': 4, 'Plywood': 4,
+        #                                        'VinylSd': 5,
+        #                                        'CemntBd': 6,
+        #                                        'Stone': 7, 'ImStucc': 7})
+        #
+        # X["MasVnrType"] = X.MasVnrType.map({'BrkCmn': 1, 'None': 1, 'BrkFace': 2, 'Stone': 3})
+        #
+        # X["ExterQual"] = X.ExterQual.map({'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4})
+        #
+        # X["Foundation"] = X.Foundation.map({'Slab': 1,
+        #                                      'BrkTil': 2, 'CBlock': 2, 'Stone': 2,
+        #                                      'Wood': 3, 'PConc': 4})
+        #
+        # X["BsmtQual"] = X.BsmtQual.map({'Fa': 2, 'None': 1, 'TA': 3, 'Gd': 4, 'Ex': 5})
+        #
+        # X["BsmtExposure"] = X.BsmtExposure.map({'None': 1, 'No': 2, 'Av': 3, 'Mn': 3, 'Gd': 4})
+        #
+        # X["Heating"] = X.Heating.map({'Floor': 1, 'Grav': 1, 'Wall': 2, 'OthW': 3, 'GasW': 4, 'GasA': 5})
+        #
+        # X["HeatingQC"] = X.HeatingQC.map({'Po': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5})
+        #
+        # X["KitchenQual"] = X.KitchenQual.map({'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4})
+        #
+        # X["Functional"] = X.Functional.map({'Maj2': 1, 'Maj1': 2, 'Min1': 2, 'Min2': 2, 'Mod': 2, 'Sev': 2, 'Typ': 3})
+        #
+        #
+        # X["GarageType"] = X.GarageType.map({'CarPort': 1, 'None': 1,
+        #                                      'Detchd': 2,
+        #                                      '2Types': 3, 'Basment': 3,
+        #                                      'Attchd': 4, 'BuiltIn': 5})
+        #
+        # X["GarageFinish"] = X.GarageFinish.map({'None': 1, 'Unf': 2, 'RFn': 3, 'Fin': 4})
+        #
+        # X["PavedDrive"] = X.PavedDrive.map({'N': 1, 'P': 2, 'Y': 3})
+        #
+        # X["SaleType"] = X.SaleType.map({'COD': 1, 'ConLD': 1, 'ConLI': 1, 'ConLw': 1, 'Oth': 1, 'WD': 1,
+        #                                  'CWD': 2, 'Con': 3, 'New': 3})
+        #
+        # X["SaleCondition"] = X.SaleCondition.map(
+        #     {'AdjLand': 1, 'Abnorml': 2, 'Alloca': 2, 'Family': 2, 'Normal': 3, 'Partial': 4})
+
+        return X
+
+
+class SkewFixer(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        numeric_columns = get_numeric_columns(X)
+
+        skewed_columns = X[numeric_columns].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skewed_columns = skewed_columns[abs(skewed_columns) > 0.75]
+        skewed_features = skewed_columns.index
+
+        for feat in skewed_features:
+            X[feat] = boxcox1p(X[feat], 0.15)
+
+        return X
+
+
+class Scaler(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        numeric_columns = get_numeric_columns(X)
+        scaler = preprocessing.StandardScaler()
+        X[numeric_columns] = scaler.fit_transform(X[numeric_columns])
+
+        return X
+
+
+class FeatureDropper(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, columns=[]):
+
+        total = X.isnull().sum().sort_values(ascending=False)
+        percent = (X.isnull().sum() / X.isnull().count()).sort_values(ascending=False)
+        missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+
+        # drop columns over 40% what fill with NAN
+        features_to_drop = missing_data[missing_data['Percent'] > 0.4].index
+        features_to_drop = features_to_drop.union(['GarageArea'])
+        X.drop(features_to_drop, axis=1, inplace=True)
+
+        return X
+
+
+class Dummyfier(TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        categorical_columns = get_categorical_columns(X)
+
+        X = pd.get_dummies(data=X, columns=categorical_columns)
+
+        return X
+
+
+class TrainDataSeparator(TransformerMixin):
+    def __init__(self, train_set_rows):
+        self.train_set_rows = train_set_rows
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        train_data = X[:self.train_set_rows]
+
+        return train_data
+
+
+def get_best_estimator(train_data, y_train_values, estimator=None, params={}, cv=5, n_jobs=-1):
+    name = estimator.__class__.__name__
+    pipeline = Pipeline(steps=[
+        (name, estimator),
+    ])
+
+    params = [
+        {
+            name+"__"+k: v for k, v in params.items()
+        }
+    ]
+    from sklearn.model_selection import cross_val_score, KFold
+    scorer = make_scorer(mean_squared_error, greater_is_better=False)
+    kf = KFold(cv, shuffle=True, random_state=42).get_n_splits(train_data)
+    grid_search = GridSearchCV(pipeline, param_grid=params, scoring=scorer, cv=kf, verbose=1, n_jobs=n_jobs)
+    grid_search.fit(train_data, y_train_values)
+
+    print("Estimator: {} score: ({}) best params: {}".format(name, sqrt(-grid_search.best_score_), grid_search.best_params_))
+    print(grid_search.best_estimator_)
+    return grid_search.best_estimator_
